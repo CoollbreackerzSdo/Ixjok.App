580dd740baee3989430afba5b198dd05b45359db
CoollbreackerzSdo <ralberinfante4@gmail.com>
2025-12-07 13:49:29 -0400

    v0.0.13 on focus in home note screen

--- Components/Principal/NoteScreen.xaml.cs
+++ Components/Principal/NoteScreen.xaml.cs
@@ -16,72 +16,74 @@ public sealed partial class NoteScreen : ContentPage
 }
 public sealed partial class NoteViewModel : BaseViewModel, IDisposable
 {
-    public NoteViewModel(IWorkSpace work, INavigationManager navigation, IAuthentication authentication) : base(navigation)
+    public NoteViewModel(IDynamicStorage storage, IAuthentication authentication, INavigationManager navigation) : base(navigation)
     {
+        _storage = storage;
         _authentication = authentication;
-        _work = work;
-        _work.Refresh += Refresh;
-        Notes ??= _work.NoteRepository.GetObservableAll();
+        Notes ??= [];
+        CloudStatusImage = "cloud_disable.png";
+        IsLoading = true;
+        _authentication.AuthenticationChange += UpdateCloudMode;
+        InitAsync().ConfigureAwait(false);
+    }
+    private async Task InitAsync()
+    {
+        IsLoading = true;
+        await Task.Delay(TimeSpan.FromMilliseconds(200));
+        Notes = _storage.GetObservableAll();
+        Notes.CollectionChanged += UpdateEmptyVisibility;
         IsNotEmpty = Notes.Any();
-        Notes.CollectionChanged += UpdateProperty;
-        CloudStatusImage = _work.Mode switch
-        {
-            StorageMode.Cloud => "cloud_enable.png",
-            _ => "cloud_disable.png"
-        };
+        IsLoading = false;
     }
     [RelayCommand]
     private async Task RefreshAsync()
     {
-        if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet && _work.Mode == StorageMode.Cloud)
-        {
-            _ = Toast.Make("Sin Internet").Show();
-            IsRefreshEnable = false;
-            return;
-        }
-        Notes = _work.NoteRepository.GetObservableAll();
-        Notes.CollectionChanged += UpdateProperty;
-        IsNotEmpty = Notes.Any();
-        CloudStatusImage = _work.Mode switch
-        {
-            StorageMode.Cloud => "cloud_enable.png",
-            _ => "cloud_disable.png"
-        };
         IsRefreshEnable = false;
     }
     [RelayCommand]
     private async Task SignAsync()
     {
-        if (await _authentication.RequestAuthenticationStateAsync() == false)
+        if (!IsConnected)
         {
             await _navigation.GoToAsync(nameof(SignInScreen));
             return;
         }
-        IsRefreshEnable = true;
-        _ = _authentication.SignOutAsync();
-        await _work.ChangeModeAsync(StorageMode.Default);
-        await Toast.Make("Session Cerrada", CommunityToolkit.Maui.Core.ToastDuration.Long).Show();
+        _ = Toast.Make("Session Cerrada", CommunityToolkit.Maui.Core.ToastDuration.Long).Show();
+        await _authentication.SignOutAsync();
     }
     [RelayCommand]
     private async Task GotoUpdateAsync(NoteModel model)
         => await _navigation.GoToAsync(nameof(NoteEditorScreen), query: new Dictionary<string, object?>()
         {
-            { nameof(EditorNoteViewModel.BaseUpdateModel) , model }
+            { nameof(EditorNoteViewModel.Model) , model }
         });
     [RelayCommand]
-    private async Task DeleteAsync(NoteModel model) => await _work.NoteRepository.RemoveAsync(model);
+    private async Task DeleteAsync(NoteModel model) => await _storage.RemoveAsync(model);
     [RelayCommand]
     private async Task GotoAddAsync() => await _navigation.GoToAsync(nameof(NoteEditorScreen));
-    private async void UpdateProperty(object? sender, NotifyCollectionChangedEventArgs e)
-        => IsNotEmpty = Notes.Any();
-    private async void Refresh() => await RefreshAsync();
+    private async void UpdateCloudMode(AuthenticationState state)
+    {
+        if (state == AuthenticationState.Connected)
+        {
+            await InitAsync();
+            CloudStatusImage = "cloud_enable.png";
+            IsConnected = true;
+        }
+        else if (state == AuthenticationState.DisConnected)
+        {
+            await InitAsync();
+            CloudStatusImage = "cloud_disable.png";
+            IsConnected = false;
+        }
+    }
+    private async void UpdateEmptyVisibility(object? sender, NotifyCollectionChangedEventArgs e) => IsNotEmpty = Notes.Any();
     private void Dispose(bool disposing)
     {
         if (!_disposedValue)
         {
             if (disposing)
             {
-                Notes.CollectionChanged -= UpdateProperty;
+                Notes.CollectionChanged -= UpdateEmptyVisibility;
             }
             _disposedValue = true;
         }
@@ -92,6 +94,8 @@ public sealed partial class NoteViewModel : BaseViewModel, IDisposable
         GC.SuppressFinalize(this);
     }
     [ObservableProperty]
+    public partial bool IsLoading { get; set; } = false;
+    [ObservableProperty]
     public partial ObservableCollection<NoteModel> Notes { get; set; }
     [ObservableProperty]
     public partial bool IsNotEmpty { get; set; }
@@ -99,7 +103,8 @@ public sealed partial class NoteViewModel : BaseViewModel, IDisposable
     public partial bool IsRefreshEnable { get; set; }
     [ObservableProperty]
     public partial ImageSource CloudStatusImage { get; set; }
+    private bool IsConnected { get; set; } = false;
     private bool _disposedValue;
+    private readonly IDynamicStorage _storage;
     private readonly IAuthentication _authentication;
-    private readonly IWorkSpace _work;
 }
