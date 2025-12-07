4e1205d6fd2b200bf460093def89343400fb90da
CoollbreackerzSdo <ralberinfante4@gmail.com>
2025-12-07 13:49:55 -0400

    v0.0.14 fixed services

--- Extensions.cs
+++ Extensions.cs
@@ -1,6 +1,9 @@
+using System.Globalization;
+using System.Threading.Channels;
 using Ixjok.Components;
 using Ixjok.Components.Principal;
 using Ixjok.Components.Sign;
+using Ixjok.Models.Note;
 using Ixjok.Services.Auth;
 using Microsoft.Extensions.Hosting;
 
@@ -12,12 +15,13 @@ public static partial class Extensions
     {
         var directoryPath = ConfigHelper.DatabaseDirectory;
         if (!Directory.Exists(directoryPath)) Directory.CreateDirectory(directoryPath);
+        // builder.Services.AddSingleton<ConfigHelper>();
         return builder;
     }
     public static IServiceCollection AddAuthentication(this IServiceCollection services)
-        => services.AddSingleton<DefaultAuthentication>()
-            .AddSingleton<IBearerAuthentication>(op => op.GetRequiredService<DefaultAuthentication>())
-            .AddSingleton<IAuthentication>(op => op.GetRequiredService<IBearerAuthentication>());
+        => services.AddSingleton<JwtHostedAuthenticationHandler>()
+            .AddSingleton<IBearerAuthenticationHandler>(op => op.GetRequiredService<JwtHostedAuthenticationHandler>())
+            .AddSingleton<IAuthentication>(op => op.GetRequiredService<IBearerAuthenticationHandler>());
     public static IServiceCollection AddScreens(this IServiceCollection services)
     {
         Routing.RegisterRoute(nameof(SignInScreen), typeof(SignInScreen));
@@ -39,6 +43,29 @@ public static partial class Extensions
         => services.AddSingleton<INavigationManager, InternalShellNavigation>();
     public static IServiceCollection AddMediaServices(this IServiceCollection services)
         => services.AddSingleton<IPictureProvider, Phone>();
-    public static IServiceCollection AddRepositories(this IServiceCollection services)
-        => services.AddSingleton<IWorkSpace, WorkSpace>();
+    public static IServiceCollection AddStorages(this IServiceCollection services)
+        => services.AddSingleton<IDynamicStorage, DynamicStorage>()
+        .AddSingleton<IHostedNoteRepository, HostedNoteStorage>();
+    extension(NoteModel model)
+    {
+        public NoteSqlDecorator ToDecorator()
+            => new()
+            {
+                Key = model.Id.ToString()!,
+                Title = model.Title,
+                Content = model.Content,
+                Registration = model.Registration.ToString()
+            };
+    }
+    extension(NoteSqlDecorator model)
+    {
+        public NoteModel ToModel()
+            => new()
+            {
+                Id = Guid.Parse(model.Key!),
+                Title = model.Title!,
+                Content = model.Content!,
+                Registration = DateTimeOffset.Parse(model.Registration!, CultureInfo.CurrentCulture)
+            };
+    }
 }
